514752b49be9f8b3096dde500a7536ec518d6445
kcnewman <newmankelvin14@gmail.com>
2025-05-18 21:08:01 +0000

    Initial project structure setup with placeholder files and directories

---
+++ init_setup.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]:%(message)s")
+
+project_name = "afrisentiment"
+
+list_of_files = [
+    "./data/.gitkeep",
+    "./data/raw",
+    "./data/processed",
+    "./data/embeddings",
+    "./data/dataloader.py",
+    "./notebooks/data_exploration.ipynb",
+    "./notebooks/model_training.ipynb",
+    "./src/__init__.py",
+    "./src/preprocessing.py",
+    "./src/embeddings.py",
+    "./src/utils.py",
+    "./src/transformation.py",
+    "./src/translation.py",
+    "./src/evaluation.py",
+    "./src/visualization.py",
+    "./models",
+    "./results/metrics",
+    "./results/figures",
+    "./tests/__init__.py",
+    "./results/.gitkeep",
+    "./requirements.txt",
+    "./main.py",
+    "./setup.py"
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    file_dir, filename = os.path.split(str(file_path))
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating directory: {file_dir} for file: {filename}")
+
+    if (not file_path.exists()) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+            logging.info(f"Creating file: {file_path}")
+    else:
+        logging.info(f"{filename} already exists.")
